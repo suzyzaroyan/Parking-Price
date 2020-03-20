574db292213e6fc47830a4211e029827f1bcd6a7
Suzy <suzy@Suzys-MacBook-Pro.local>
2020-03-21 00:22:21 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# User Story 2: After getting desired duration of parking entered by the user, the rate for the parking is calculated below using user's input given above.
+
+
+rate=100 # rate is 100 drams per hour
+if answer == time[0]:
+  print('Rate for parking is ', rate * 0, ' drams')
+elif answer == time[1]:
+  print('Rate for parking is ', rate * 1, ' drams')
+elif answer == time[2]:
+  print('Rate for parking is ', rate * 2, ' drams')
+elif answer == time[3]:
+  print('Rate for parking is ', rate * 3, ' drams')
+elif answer == time[4]:
+  print('Rate for parking is ', rate * 4, ' drams')
+elif answer == time[5]:
+  print('Rate for parking is ', rate * 5, ' drams')
+elif answer == time[6]:
+  print('Rate for parking is ', rate * 6, ' drams')
+else:
+  print('Something is wrong')
